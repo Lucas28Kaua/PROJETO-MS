d22e949ce8b468df6a2d3a45de01599fe22ac460
Kauã <lkaua1048@gmail.com>
2026-01-01 18:19:51 -0300

    atualizações

---
+++ javascript/login.js
@@ -0,0 +1,8 @@
+function logar(){
+    const email = window.document.getElementById('iuser').value;
+    const senha = window.document.getElementById('isenha').value;
+
+    if (email&&senha){
+        window.location.href = ''
+    }
+}
