e7114a0e913566efb1dd85bcd3678b41d4a9f2e2
Kauã <lkaua1048@gmail.com>
2026-01-01 18:46:17 -0300

    link p tela home e validação de dominio de email

--- javascript/login.js
+++ javascript/login.js
@@ -1,8 +1,16 @@
 function logar(){
     const email = window.document.getElementById('iuser').value;
     const senha = window.document.getElementById('isenha').value;
+    const dominiosValidos = ['@gmail.com', '@hotmail.com', '@outlook.com'];
+    const emailValido = dominiosValidos.some(dominio => email.toLowerCase().endsWith(dominio));
 
-    if (email&&senha){
-        window.location.href = ''
+    if (!email || !senha){
+        alert('Preencha todos os campos!');
+    } else if (!emailValido){
+        alert('Digite um Email Válido!')
+    } else if(senha.length<8){
+        alert('Digite uma senha de 8 dígitos!')
+    } else{
+        window.location.href='../paginashtml/telahome.html'
     }
 }
