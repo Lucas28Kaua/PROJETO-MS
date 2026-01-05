6d1377703ed708d499085819ccfd93a028519cc9
Kauã <lkaua1048@gmail.com>
2026-01-05 15:28:30 -0300

    criacao tela consulta html css js

---
+++ javascript/consulta.js
@@ -0,0 +1,24 @@
+const toggleBtn = document.getElementById('toggleMenu');
+const sidebar = document.querySelector('.sidebar');
+
+toggleBtn.addEventListener('click', function() {
+    sidebar.classList.toggle('aberto');
+});
+
+const menuToggle = document.getElementById('menuToggle');
+const overlay = document.getElementById('overlay');
+
+if (menuToggle) {
+    menuToggle.addEventListener('click', function() {
+        sidebar.classList.toggle('aberto');
+        menuToggle.classList.toggle('ativo');
+        overlay.classList.toggle('ativo');
+    });
+    
+    // Fecha ao clicar no overlay
+    overlay.addEventListener('click', function() {
+        sidebar.classList.remove('aberto');
+        menuToggle.classList.remove('ativo');
+        overlay.classList.remove('ativo');
+    });
+}
