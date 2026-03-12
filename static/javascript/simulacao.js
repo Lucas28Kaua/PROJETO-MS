de9e19e0750fd7e2f6788947fd8cb87f19d93606
Kauã <lkaua1048@gmail.com>
2026-03-12 15:27:18 -0300

    add js

---
+++ static/javascript/simulacao.js
@@ -0,0 +1,4 @@
+const toggleBtn = document.getElementById('toggleMenu');
+const sidebar = document.querySelector('.sidebar');
+const menuToggle = document.getElementById('menuToggle');
+const overlay = document.getElementById('overlay');
